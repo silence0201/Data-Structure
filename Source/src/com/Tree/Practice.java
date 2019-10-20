38244636edfe78589216e58f9cd3f23e42ca90e5
Silence <374619540@qq.com>
2019-10-20 20:09:26 +0800

    invertTree

---
+++ Source/src/com/Tree/Practice.java
@@ -0,0 +1,89 @@
+package com.Tree;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by silence on 2019/10/20.
+ */
+
+ class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+
+public class Practice {
+
+        // 层次遍历
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) return root;
+
+            Queue<TreeNode> queue = new LinkedList<>();
+
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.poll();
+
+                TreeNode tmp = node.left;
+                node.left = node.right;
+                node.right = tmp;
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            return root;
+        }
+
+    // 前序遍历
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) return root;
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree2(root.left);
+        invertTree2(root.right);
+
+        return root;
+    }
+
+    // 后续遍历
+    public TreeNode invertTree3(TreeNode root) {
+        if (root == null) return root;
+
+        invertTree3(root.left);
+        invertTree3(root.right);
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        return root;
+    }
+
+    // 中序遍历
+    public TreeNode invertTree4(TreeNode root) {
+        if (root == null) return root;
+
+        invertTree4(root.left);
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree4(root.left);
+
+
+
+        return root;
+    }
+}
